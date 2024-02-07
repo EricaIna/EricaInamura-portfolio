fbfe0d290a2e161d8b4afa2d0b25071598c2183d
“EricaIna” <erica.rirura.4you@gmail.com>
2024-02-07 23:26:50 +0000

    img path fix

---
+++ src/components/AboutMe/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+// import "./AboutMe.css";
+
+const AboutMe = () => {
+  return (
+    <div className="AboutMe">
+      <h1>ABOUT ME</h1>
+      <p className="aboutme-p">
+        My passion for technology started when I was working as a nurse,
+        especially during the pandemic. <br></br>I realized the crucial role of
+        learning technology in improving both the workflow for healthcare
+        professionals and the overall experience for patients. As our world
+        continues to rapidly evolve in the midst of technological advancements,
+        I find myself constantly thinking about innovative ways to adapt and
+        enhance the intersection of technology and healthcare.My desire for
+        self- improvement and passion for gaining new knowledge have become
+        essential elements in my personal and professional development.
+      </p>
+    </div>
+  );
+};
+
+export default AboutMe;
