31e9b47c38f399bb8a95b56f1c6d111e1a28a554
“EricaIna” <erica.rirura.4you@gmail.com>
2024-02-12 13:07:03 +0000

    section height adjust

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -15,12 +15,11 @@ const AboutMe = () => {
         My passion for technology started when I was working as a nurse,
         especially during the pandemic. <br></br>I realized the crucial role of
         learning technology in improving both the workflow for healthcare
-        professionals and the overall experience for patients. As our world
-        continues to rapidly evolve in the midst of technological advancements,
-        I find myself constantly thinking about innovative ways to adapt and
-        enhance the intersection of technology and healthcare.My desire for
-        self- improvement and passion for gaining new knowledge have become
-        essential elements in my personal and professional development.
+        professionals and the overall experience for patients. Driven by a
+        passion to learn and improve, I'm committed to lifelong learning for
+        personal and professional growth.Additionally, I find joy in design,
+        often drawing inspiration from museum visits or traveling around to fuel
+        my creative endeavors.
       </p>
     </motion.div>
   );
